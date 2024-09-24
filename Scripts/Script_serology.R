51c901a76485cc5cde311893b2d7e2945304404a
Sara Wijburg <wijburgs@rivm-lsfsv-l13p.rivm.ssc-campus.nl>
2024-09-24 09:47:17 +0200

    analyse deel toegevoegd

--- Scripts/Script_serology.R
+++ Scripts/Script_serology.R
@@ -10,3 +10,11 @@ serology_data_org2 <- read_excel("Data/serology_data.xlsx", sheet = "lamb_info")
 
 #------------------ Clean
 source(file = "Scripts/Script_serology_clean.R")
+
+#------------------ Analyses
+# Onset of the immune response: Whether there's a difference in the time at which the immune response becomes noticeable between the groups.
+# Severity of the immune response: Whether there is a difference in the intensity or severity of the immune response between groups over time.
+source(file = "Scripts/Script_serology_analysis.R")
+
+
+
